549979f90a4e0ed192c0df8588010de21f67638e
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-05-22 14:59:19 +0200

    init(): Starting the django api project

---
+++ API/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    size = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
